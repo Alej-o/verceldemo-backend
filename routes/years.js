5b76cf63b49f63cfeb607eacf9b08f376d4e0c70
Your name <Your email>
2025-01-24 11:24:59 +0100

    whatyearisit

---
+++ routes/years.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+  const date = new Date().getFullYear();
+ res.json({ now: date });
+});
+
+module.exports = router;
